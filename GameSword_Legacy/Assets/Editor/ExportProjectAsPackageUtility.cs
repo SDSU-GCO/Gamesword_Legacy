ea64e240054957b8ce0ed65b8fa1d30eb4e0217e
jackdholm <jackdholm1@gmail.com>
2017-04-22 01:56:30 -0500

    EnemyMovement
    Whatever

---
+++ GameSword_Legacy/Assets/Editor/ExportProjectAsPackageUtility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExportProjectAsPackageUtility : MonoBehaviour
+{
+        [MenuItem("Export/Export Project as Package")]
+    static void export()
+    {
+        var path = EditorUtility.SaveFilePanel("Export Project as UnityPackage", "",
+                 "New UnityPackage.unitypackage",
+                 "unitypackage");
+
+        if (path.Length != 0)
+        {
+            AssetDatabase.ExportPackage(AssetDatabase.GetAllAssetPaths(), path, ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
+        }
+
+        
+    }
+}
---
+++ GameSword_Legacy/Assets/Editor/HiResTexturePostProcessor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+//Code from http://answers.unity3d.com/questions/55118/changing-texture-import-default-settings.html
+public class HiResTexturePostProcessor : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter importer = assetImporter as TextureImporter;
+        importer.maxTextureSize = 8192;
+        importer.compressionQuality = 100;
+        importer.textureCompression = TextureImporterCompression.CompressedHQ;
+
+        Object asset = AssetDatabase.LoadAssetAtPath(importer.assetPath, typeof(Texture2D));
+        if (asset)
+        {
+            EditorUtility.SetDirty(asset);
+        }
+        else
+        {
+            importer.maxTextureSize = 8192;
+            importer.compressionQuality = 100;
+            importer.textureCompression = TextureImporterCompression.CompressedHQ;
+        }
+    }
+}
